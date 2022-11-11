f3118ab4ec14544a8ced387be257c0895acba363
Nishijama <nathanplociennik@gmail.com>
2022-11-11 01:26:11 +0100

    updated js to handle tile description hover changed layout for mobile

--- index.js
+++ index.js
@@ -8,7 +8,22 @@ let images = document.querySelectorAll('.img-miniature')
 let snippets = document.querySelectorAll('.project-description')
 let tiles = document.querySelectorAll('.tile')
 
-console.log(tiles);
+tiles.forEach(tile => {
+    let image = tile.children[0];
+    let description = tile.children[1];
+    description.style.visibility = 'hidden';
+
+    tile.addEventListener('mouseover', () => {
+        description.style.visibility = 'visible';
+        description.style.opacity = 1;
+    });
+
+
+    tile.addEventListener('mouseout', () => {
+        description.style.opacity = 0;
+        description.style.visibility = 'hidden';
+    })
+})
 
 
 // CANVAS
@@ -138,4 +153,3 @@ function toggleHeader() {
     document.querySelector("#avatar").classList.toggle("hidden")
     document.querySelector("#header-text").classList.toggle("hidden")
 }
-
